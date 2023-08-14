0c7c65a7b9ecb8d564aa294befd73794684fb854
harshraj29 <rajh3282@gmail.com>
2023-08-14 23:25:20 +0530

    Config all the files and Done the browser routes

---
+++ src/Pages/TeamIei.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TeamIei() {
+  return (
+    <div className='h-screen bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black flex items-center justify-center'>
+
+      <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-7xl">
+        Team IEI
+      </h1>
+    </div>
+  )
+}
+
+export default TeamIei
