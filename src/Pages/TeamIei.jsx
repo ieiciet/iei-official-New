e427bf0ac35789cd64ae995e9ec33c02f6ba2add
VanshArora08 <kumar.vansharora@gmail.com>
2023-08-16 01:47:18 +0530

    added some gradients

--- src/Pages/TeamIei.jsx
+++ src/Pages/TeamIei.jsx
@@ -10,7 +10,7 @@ function TeamIei() {
       <h1 className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">
         Meet Our Team
       </h1>
-      <h1 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
+      <h1 className="orange_gradient font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
         Team IE(I)
       </h1>
       <section className="flex flex-col justify-center items-center py-10">
