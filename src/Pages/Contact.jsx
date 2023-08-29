b373f5dbbb888d9c7f095dcc0189c3d551b10f39
unknown <munishgoel45698@gmail.com>
2023-08-29 11:46:08 +0530

    Contact us

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,14 +1,77 @@
-import React from 'react'
+import React from 'react';
 
 function Contact() {
   return (
-    <div className='h-screen bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black flex items-center justify-center'>
-
-      <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-7xl">
-        Contact
-      </h1>
-    </div>
-  )
+    
+     <main className="relative py-28 bg-gray-900" style={{backgroundColor: "#1d1a1a"}} >
+            <div className="relative z-10 max-w-screen-xl mx-auto text-gray-600 sm:px-4 md:px-8">
+                <div className="max-w-lg space-y-3 px-4 sm:mx-auto sm:text-center sm:px-0">
+                    <h3 className="text-cyan-400 font-semibold">
+                        {/* Contact */}
+                    </h3>
+                    <p className="text-white text-3xl font-semibold sm:text-4xl">
+                        Get in touch
+                    </p>
+                    <p className="text-gray-300">
+                        We’d love to hear from you! Please fill out the form bellow.
+                    </p>
+                </div>
+                <div className="mt-12 mx-auto px-4 p-8 bg-white sm:max-w-lg sm:px-8 sm:rounded-xl">
+                    <form
+                        onSubmit={(e) => e.preventDefault()}
+                        className="space-y-5"
+                    >
+                        <div>
+                            <label className="font-medium">
+                                Full name
+                            </label>
+                            <input
+                                type="text"
+                                required
+                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+                            />
+                        </div>
+                        <div>
+                            <label className="font-medium">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                required
+                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+                            />
+                        </div>
+                        <div>
+                            <label className="font-medium">
+                                Phone number
+                            </label>
+                            <div className="relative mt-2">
+                             
+                                <input
+                                    type="number"
+                                    style={{padding:"1%"}}
+                                    required
+                                    className="w-full pl-[4.5rem] pr-3 py-2 appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <label className="font-medium">
+                                Message
+                            </label>
+                            <textarea required className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"></textarea>
+                        </div>
+                        <button
+                            className="w-full px-4 py-2 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-lg duration-150"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div className='absolute inset-0 blur-[118px] max-w-lg h-[800px] mx-auto sm:max-w-3xl sm:h-[400px]' style={{ background: "linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)" }}></div>
+        </main>
+  );
 }
 
-export default Contact
+export default Contact;
