628be8d64163f52f675c087ed9c7bf8e27203f2f
harshraj29 <rajh3282@gmail.com>
2023-09-11 02:13:14 +0530

    No Upcomming event Added

--- src/components/PastEvent.jsx
+++ src/components/PastEvent.jsx
@@ -46,7 +46,7 @@ function PastEvent() {
     <div className='min-h-screen bg-[#121212] sm:px-20 xl:px-36 px-10 pb-10 pt-24'>
       <div className='main-p min-h-full'>
         <div className="flex flex-col items-center justify-center">
-          <div className="text-white px-6 font-bold md:text-[40px] font-poppins">Past Event</div>
+          <div className="text-white px-6 font-bold text-[24px] md:text-[40px] font-poppins">Past Event</div>
           <div className='w-full h-[1px] bg-white mb-10'></div>
         </div>
 
