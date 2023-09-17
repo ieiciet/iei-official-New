c9fcb73472be0f4d245ec06d818e3a6ec4a36c03
Aryan Shubbu <102131199+LoneExpert@users.noreply.github.com>
2023-09-18 01:19:39 +0530

    Add files via upload
    About Page Done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Component/Navbar';
+// import Slider from './Component/Slider';
+
+function App() {
+  return (
+    <div className='head'>
+      <Navbar/>
+      {/* <Slider/> */}
+    </div>
+  );
+}
+
+export default App;
