86b2be589b99a29b383d28f89bb33238c983602d
VanshArora08 <kumar.vansharora@gmail.com>
2023-09-27 22:29:31 +0530

    added about and working recent events autoplay

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,19 +3,8 @@ import heroBg from '../assets/bg/bgHero.png'
 import { Link } from 'react-router-dom';
 import '../styles/home.css'
 import SectionBg1 from "../assets/bg/sectionBg1.png"
+import RecentEvents from '../components/recentEvents';
 
-import { useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
-
-
-import 'swiper/css';
-import 'swiper/css/effect-cards';
-
-import '../styles/slider.css';
-
-// import required modules
-import { EffectCards } from 'swiper/modules';
 
 // end slider
 
@@ -41,31 +30,19 @@ function Home() {
           </div>
         </div>
       </section>
-      <section className='sm:px-20 xl:px-36 px-10 bg-bg w-full pt-14 pb-14 h-[100vh]'>
+      <section className='sm:px-20 xl:px-36 px-10 bg-bg w-full pt-14 pb-14 h-[100vh] flex'>
+        <div className='flex flex-col py-11 w-[60%] gap-6 pr-9 justify-evenly'>
+          <h1 className='text-white sm:text-left font-poppins font-bold text-[44px] text-center mb-4'>About IE(I)</h1>
+          <p className='text-gray-400 sm:text-left w-full text-center text-[18px] font-poppins'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ea unde, nisi, veniam asperiores mollitia aliquid obcaecati quis nostrum quaerat nihil ipsam ut! Esse eaque repudiandae iusto quos. Perferendis, officiis.</p>
+          <p className='text-gray-400 sm:text-left w-full text-center text-[18px] font-poppins'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ea unde, nisi, veniam asperiores mollitia aliquid obcaecati quis nostrum quaerat nihil ipsam ut! Esse eaque repudiandae iusto quos. Perferendis, officiis.</p>
+          <p className='text-gray-400 sm:text-left w-full text-center text-[18px] font-poppins'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ea unde, nisi, veniam asperiores mollitia aliquid obcaecati quis nostrum quaerat nihil ipsam ut! Esse eaque repudiandae iusto quos. Perferendis, officiis.</p>
+        </div>
+        <div className='green-pink-gradient h-full w-[40%] rounded-md'>
+
+        </div>
 
       </section>
-      <section className='sm:px-20 xl:px-36 px-10 bg-bg flex w-full flex-col sm:flex-row justify-evenly pt-14 pb-14 h-fit second-section'>
-        <div className='w-full sm:w-[55%] flex flex-col justify-center items-center relative'>
-        <Swiper
-          effect={'cards'}
-          grabCursor={true}
-          modules={[EffectCards,Autoplay]}
-          autoplay={{ delay: 3500 }}
-          className="mySwiper"
-        >
-          {events.map((event,index) => (
-            <SwiperSlide key={index}>
-              <img src={event} alt="event" className='w-full h-full'/>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        </div>
-        <div className='w-full sm:w-[45%] flex flex-col justify-center gap-14 sm:items-end items-center pt-10 sm:pt-0'>
-          <h1 className='text-white sm:text-right font-poppins font-semibold text-[44px] text-center'>Our Recent Events</h1>
-          <p className='text-gray-400 sm:text-right w-full text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate hic natus  doloribus! Magnam eos ipsa, nam pariatur fuga dolores dolorem ipsam quae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora at quibusdam illo eaque. At, expedita! Quidem esse facilis ullam debitis, sunt eligendi. Error reiciendis a non eum quas dolore voluptas.</p>
-          <Link to='/pastEvent' className='green-pink-gradient rounded-full p-3 pr-10 pl-10 w-fit'>View All Events</Link>
-        </div>
-      </section>
+      <RecentEvents/>
       
     </div>
   )
