339d1ba50aa3212b1520b99c1fe9d01377708367
harshraj29 <rajh3282@gmail.com>
2023-10-30 01:43:54 +0530

    Recent Image Updated

--- src/utils/pastEvents.js
+++ src/utils/pastEvents.js
@@ -3,7 +3,10 @@ import CodeXelerate from '../assets/eventImages/CodeXelerate.jpg'
 import SquidCarnival from '../assets/eventImages/SquidCarnival.jpg'
 import ValueFit from '../assets/eventImages/valueFit.jpg'
 import Kickstart from '../assets/eventImages/kickstartStartup.jpg'
+import PowerClash from '../assets/eventImages/POWER_CLASH.png'
+import GraphicFusion from '../assets/eventImages/Graphics.jpg'
+import MindFizz from '../assets/eventImages/Mindfizz.jpg'
 
 export const events=[
-    CodeXelerate,SquidCarnival,ValueFit,Kickstart,WebWisdom
+    MindFizz,GraphicFusion,CodeXelerate,SquidCarnival,PowerClash
 ]
